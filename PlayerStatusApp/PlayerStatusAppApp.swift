1f44f836aa7ec2515d672c974b354b404b0e1171
Taichone <taichone.dev+1@gmail.com>
2024-01-11 13:11:06 +0900

    Initial Commit

---
+++ PlayerStatusApp/PlayerStatusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerStatusAppApp.swift
+//  PlayerStatusApp
+//
+//  Created by Taichi on 2024/01/11.
+//
+
+import SwiftUI
+
+@main
+struct PlayerStatusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
